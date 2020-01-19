08a1099bca1b82d37c2d7bc1a8b880a87b66e57b
Roger Vilà <rogervila@me.com>
2020-01-19 14:19:08 +0100

    Initial setup

---
+++ src/marquee.js
@@ -0,0 +1,50 @@
+export default class Marquee {
+    constructor(props) {
+        this.validate(props)
+        this.props = props
+        this.setElement()
+        this.setChild()
+    }
+
+    setElement() {
+        this.element = document.querySelector(this.props.element)
+        // this.element.style.overflow = 'hidden'
+    }
+
+    validate(props) {
+        [
+            { name: 'element', type: '' },
+            { name: 'velocity', type: 0 }
+        ].forEach((valid) => {
+            if (!props[valid.name] || typeof props[valid.name] !== typeof valid.type) {
+                throw new TypeError(`"${valid.name}" property is mandatory and must be a type of "${typeof valid.type}"`)
+            }
+        })
+    }
+
+    setChild() {
+        this.child = this.element.firstElementChild
+        // this.child.style.display = 'inline-flex'
+        // this.child.style.position = 'relative'
+        this.child.style.right = '-' + this.element.offsetWidth + 'px';
+    }
+
+    run() {
+        // console.log('run? ' + (new Date()).getTime())
+        let rightPosition = parseFloat(this.child.style.right);
+        rightPosition += this.props.velocity
+
+        let parentOffsetWidth = this.element.offsetWidth
+        let childOffsetWidth = this.child.offsetWidth
+
+        if (rightPosition > childOffsetWidth) {
+            rightPosition = -parentOffsetWidth;
+        }
+
+        if (null === this.element.querySelector(':hover')) {
+            this.child.style.right = rightPosition + 'px'
+        }
+
+        requestAnimationFrame(this.run.bind(this));
+    }
+}
---
+++ test/marquee.test.js
@@ -0,0 +1,69 @@
+const Marquee = require('../src/marquee.js').default
+
+describe('Marquee Validation', () => {
+    it('should fail if a prop is missing', async () => {
+        try {
+            new Marquee({
+                element: '#test'
+                // velocity is missing
+            })
+        } catch (e) {
+            expect(e instanceof TypeError).toBe(true);
+        }
+
+        try {
+            new Marquee({
+                // element is missing
+                velocity: 1
+            })
+        } catch (e) {
+            expect(e instanceof TypeError).toBe(true);
+        }
+    })
+
+    it('should fail if a prop type is not valid', async () => {
+        try {
+            new Marquee({
+                element: '#test',
+                velocity: 'not a number'
+            })
+        } catch (e) {
+            expect(e instanceof TypeError).toBe(true);
+        }
+
+        try {
+            new Marquee({
+                element: ['not a string'],
+                velocity: 1
+            })
+        } catch (e) {
+            expect(e instanceof TypeError).toBe(true);
+        }
+    })
+
+    it('should run if html content is correct', async () => {
+        document.body.innerHTML = `<div id="test"><div>content</div></div>`
+
+        const m = new Marquee({
+            element: '#test',
+            velocity: 1
+        })
+
+        m.run()
+    })
+
+    it('should fail if html content is incorrect', async () => {
+        document.body.innerHTML = `<div id="test">missing child element</div>`
+
+        try {
+            const m = new Marquee({
+                element: '#test',
+                velocity: 1
+            })
+
+            m.run()
+        } catch (e) {
+            expect(e.message === `Cannot read property 'style' of null`).toBe(true);
+        }
+    })
+});
